eba1715740773e8a0c059e8e9492e43240a49831
Abdurrahman Hac Said <abdulrahman.hajsaid.98@gmail.com>
2024-07-01 12:18:34 +0300

    01.07.2024 sth (1)

--- Dtos/UserAddDto.cs
+++ Dtos/UserAddDto.cs
@@ -5,14 +5,27 @@ namespace WeeklyProgram.Dtos
 {
     public class UserAddDto
     {
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         public string? FirstName { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         public string? LastName { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         public string?  PhoneNumber { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         [DataType(DataType.Password)]
         public string?  Password { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         public Guid RoleId { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل ضروري")]
         public List<AppRole>? Roles { get; set; }
     }
 }
